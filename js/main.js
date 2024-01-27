546a28b5b685a732f80a9d878bb9c3b0588e859d
suri30715 <suri30715@gmail.com>
2024-01-27 12:16:02 +0900

    24.01.26

--- js/main.js
+++ js/main.js
@@ -3,14 +3,28 @@ function init()
     
 }
 
-function setTitleBox(boxNumberId, imgSrc, title, paragraph)
+function setCardItem(cardItemId, imgSrc, paragraph, link)
 {
-    $(".title-box-wrapper").append('<div class = "title-box"' + 'id =' + '"' + boxNumberId + '"' + '>' + '<img src = ' + imgSrc + '><br>' + title +'<br>' + paragraph + '</div>' );
+    $(".card-list")
+    .append
+    (
+        '<a href =' + link + ' ' + 'class = card-item id =' + cardItemId + '>' + 
+            '<figure class = thumbnail>' +
+                // '<div class = img></div>' + 
+                '<img src = ' + imgSrc + '>' + 
+            '</figure>' +
+            '<div class = thumbnail-info>' + paragraph + '</div>' +
+        '</a>'
+    );
+    // 이거 왜 &quout; 뜨는교? 일단 img 태그로 바꿈
+    //$(".img").css("background-image" , imgSrc);
 }
 
 
 init();
 
 // test
-setTitleBox(1, "../img/ue53.png", "Portfolio", "포트폴리오 입니다. ");
-setTitleBox(2, "../img/ue53.png", "Portfolio", "포트폴리오 입니다. ");
+setCardItem(1, "../img/ue53.png", "UE4포트폴리오", "../html/ue5.html");
+setCardItem(2, "../img/ue53.png", "UE5포트폴리오", "../html/ue5.html");
+setCardItem(3, "../img/sg1.png", "SG스텝스 홈페이지", "../html/ue5.html");
+setCardItem(4, "../img/ue53.png", "C++", "../html/ue5.html");
