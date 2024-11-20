8ad40d917188224ede5aaffaab3dfe9d5bdbd8ad
suri30715 <suri30715@gmail.com>
2024-11-20 17:01:52 +0900

    24.11.20
    renewal

--- js/common.js
+++ js/common.js
@@ -1,9 +1,7 @@
 function init()
 {
-    $("header").load("../html/header.html")
+    $(".header").load("../html/header.html")
     $(".sidebar").load("../html/sidebar.html");
-    $(".content").load("../html/content.html");
-    $(".console").load("../html/console.html");
 }
 
 init();
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,81 +1,107 @@
 function init()
 {
-    if(document.getElementById("ue4"))
+    getTitleSmall();
+}
+
+function getTitleSmall()
+{
+    const content2List = document.querySelectorAll(".content");
+    const index = document.querySelector("#index");
+    var content2HList = [];
+    const divisionList = new Array(content2List.length);
+    
+    document.getElementById("count").innerHTML = "글: " + content2List.length;
+
+    for(let i = 0; i < content2List.length; i++)
     {
-        document.getElementById("ue4").addEventListener("click", event=>{window.location.href="../html/ue4.html"})
-    }   
-    if(document.getElementById("ue5"))
-    {
-        document.getElementById("ue5").addEventListener("click", event=>{window.location.href="../html/ue5.html"})
-    }   
-    if(document.getElementById("M-Unreal"))
-    {
-        document.getElementById("M-Unreal").addEventListener("click", event=>{window.location.href="../html/m-unreal.html"})
+        content2HList = content2List[i].querySelectorAll("h1, h2, h3, h4, h5, h6");
+
+        divisionList[i] = document.createElement("div");
+        divisionList[i].setAttribute("class", "division");
+        index.appendChild(divisionList[i]);
+
+        if(content2HList)
+        {
+            for(let j = 0; j < content2HList.length; j++)
+            {
+                if(content2HList[j].tagName == "H1")
+                {
+                   var c = document.createElement("div");
+                   c.setAttribute("class", "one");
+                   c.innerHTML = content2HList[j].textContent;
+                   divisionList[i].appendChild(c);
+                }
+                if(content2HList[j].tagName == "H2")
+                {
+                    var c = document.createElement("div");
+                    c.setAttribute("class", "two");
+                    c.innerHTML = content2HList[j].textContent;
+                    divisionList[i].appendChild(c);
+                }
+                if(content2HList[j].tagName == "H3")
+                {
+                    var c = document.createElement("div");
+                    c.setAttribute("class", "three");
+                    c.innerHTML = content2HList[j].textContent;
+                    divisionList[i].appendChild(c);
+                }
+                if(content2HList[j].tagName == "H4")
+                {
+                    var c = document.createElement("div");
+                    c.setAttribute("class", "four");
+                    c.innerHTML = content2HList[j].textContent;
+                    divisionList[i].appendChild(c);
+                }
+                if(content2HList[j].tagName == "H5")
+                {
+                    var c = document.createElement("div");
+                    c.setAttribute("class", "five");
+                    c.innerHTML = content2HList[j].textContent;
+                    divisionList[i].appendChild(c);
+                }
+                if(content2HList[j].tagName == "H6")
+                {
+                    var c = document.createElement("div");
+                    c.setAttribute("class", "six");
+                    c.innerHTML = content2HList[j].textContent;
+                    divisionList[i].appendChild(c);
+                }
+            }
+        }
     }
-    if(document.getElementById("M-Unity"))
+
+    // 목차의 내용 클릭시 해당 위치로 스크롤
+    const indexList = document.querySelectorAll(".two, .three, .four, .five, .six");
+    const hList = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+    
+    for(let i = 0; i < indexList.length; i++)
     {
-        document.getElementById("M-Unity").addEventListener("click", event=>{window.location.href="../html/m-unity.html"})
-    }    
-    if(document.getElementById("sg"))
-    {
-        document.getElementById("sg").addEventListener("click", event=>{window.location.href="../html/sg.html"})
-    }   
-    if(document.getElementById("c++"))
-    {
-        document.getElementById("c++").addEventListener("click", event=>{window.location.href="../html/c++.html"})
+        indexList[i].addEventListener
+        (
+            "click", 
+            function()
+            {
+                // https://enai.tistory.com/33
+                window.scrollTo(0, hList[i].offsetTop);
+            }
+        );
     }
-    // if(document.getElementById("c#"))
-    // {
-    //     document.getElementById("c#").addEventListener("click", event=>{window.location.href="../html/csharp.html"})
-    // }
-    if(document.getElementById("stl"))
-    {
-        document.getElementById("stl").addEventListener("click", event=>{window.location.href="../html/stl.html"})
-    }
-    if(document.getElementById("csharp"))
-    {
-        document.getElementById("csharp").addEventListener("click", event=>{window.location.href="../html/csharp.html"})
-    }   
-    if(document.getElementById("book:-easy-c-d&a"))
-    {
-        document.getElementById("book:-easy-c-d&a").addEventListener("click", event=>{window.location.href="../html/d&a.html"})
-    }   
-    if(document.getElementById("book:-Lee-math"))
-    {
-        document.getElementById("book:-Lee-math").addEventListener("click", event=>{window.location.href="../html/math.html"});
-    }
-    if(document.getElementById("graphics"))
-    {
-        document.getElementById("graphics").addEventListener("click", event=>{window.location.href="../html/graphics.html"})
-    }   
-    if(document.getElementById("cs"))
-    {
-        document.getElementById("cs").addEventListener("click", event=>{window.location.href="../html/cs.html"})
-    }
-    if(document.getElementById("cs-mine"))
-    {
-        document.getElementById("cs-mine").addEventListener("click", event=>{window.location.href="../html/cs-mine.html"})
-    }
-    if(document.getElementById("js"))
-    {
-        document.getElementById("js").addEventListener("click", event=>{window.location.href="../html/js.html"})
-    }
-    if(document.getElementById("css"))
-    {
-        document.getElementById("css").addEventListener("click", event=>{window.location.href="../html/css.html"})
-    }
-    if(document.getElementById("html"))
-    {
-        document.getElementById("html").addEventListener("click", event=>{window.location.href="../html/html.html"})
-    }
-    if(document.getElementById("unity"))
-    {
-        document.getElementById("unity").addEventListener("click", event=>{window.location.href="../html/unity.html"})
-    }
-    if(document.getElementById("go-main"))
-    {
-        document.getElementById("go-main").addEventListener("click", event=>{window.location.href="../html/main.html"})
-    }
-}   
+
+    window.addEventListener
+    (
+        "scroll",
+        function()
+        {
+            for(let i = 0; i < indexList.length; i++)
+            {
+                // https://velog.io/@nemo/scroll-offset-property
+                if(scrollY >= hList[i].offsetTop)
+                    indexList[i].style.color = "#30b565";
+                else
+                    indexList[i].style.color = "black";
+            }
+        }
+    );
+}
 
 init();
